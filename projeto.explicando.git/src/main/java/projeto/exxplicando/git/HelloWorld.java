acfb137139edbead23782ea2732bed4112f35ceb
alunoziero <abarbosa.e2e@gmail.com>
2021-12-02 19:49:25 -0300

    alteracao

--- projeto.explicando.git/src/main/java/projeto/exxplicando/git/HelloWorld.java
+++ projeto.explicando.git/src/main/java/projeto/exxplicando/git/HelloWorld.java
@@ -4,13 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		int num1 = 10;
-		
-		if (num1 == 10) {
-			System.out.println("numeros iguais");
-		}
-		
-		System.out.println("Hello World");
+		System.out.println("Alterado");
 		
 		
 	}
