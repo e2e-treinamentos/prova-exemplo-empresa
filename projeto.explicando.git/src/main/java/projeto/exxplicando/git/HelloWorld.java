7802f7dee901e89b5d2c3fd6933377b4bd450c7d
alunoziero <abarbosa.e2e@gmail.com>
2021-12-02 19:47:02 -0300

    inclusao do projeto

---
+++ projeto.explicando.git/src/main/java/projeto/exxplicando/git/HelloWorld.java
@@ -0,0 +1,18 @@
+package projeto.exxplicando.git;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		
+		if (num1 == 10) {
+			System.out.println("numeros iguais");
+		}
+		
+		System.out.println("Hello World");
+		
+		
+	}
+
+}
